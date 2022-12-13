0ba782588153cfc993a1902fdf53f119591bd479
kathynguuyen <kathynguuyen@gmail.com>
2022-12-14 02:38:08 +0900

    tickets + index update

--- myscript.js
+++ myscript.js
@@ -15,3 +15,11 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+
+// TICKETS BOOKING
+
+
+function submitForm() {
+        alert("Thank you for booking your ticket!");
+      }
